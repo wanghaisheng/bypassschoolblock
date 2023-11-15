8021897301d348218f96ec2afa54a0fe1b87c043
s1871012 <118460956+s1871012@users.noreply.github.com>
2023-11-15 14:55:42 -0600

    Update cloak.js

--- cloak.js
+++ cloak.js
@@ -29,7 +29,7 @@ class ABC {
 					}
 				} else if (this.type == "overwrite") {
 					try {
-						document.body.innerHTML = `<script>window.onhashchange = function() {if (window.innerDocClick) {window.innerDocClick = false;} else {if (window.location.hash != '#undefined') {alert("back");} else {history.pushState("", document.title, window.location.pathname);location.reload();}}}</script>
+						document.body.innerHTML = `
 	<iframe
 		style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none"
 		sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"
