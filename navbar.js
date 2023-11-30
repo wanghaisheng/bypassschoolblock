e020836e71cc50e9343d95953dde6e548778649e
s1871012 <118460956+s1871012@users.noreply.github.com>
2023-11-30 22:11:24 +0000

    modified:   category/3d.html

--- navbar.js
+++ navbar.js
@@ -1,17 +1,3 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('../nav.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-                alert('err')
-            }
-            return response.text();
-        })
-        .then(data => {
-            document.body.insertAdjacentHTML('afterbegin', data);
-        })
-        .catch(error => {
-            alert('err')
-            console.error('There has been a problem with your fetch operation: ', error);
-        });
+$(function () {
+    $("#nav-placeholder").load("../nav.html");
 });
