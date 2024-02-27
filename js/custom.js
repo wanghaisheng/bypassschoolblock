79ef8e99e0f25888039d58183588182dcebf9798
kandurz <118460956+kandurz@users.noreply.github.com>
2024-02-27 13:57:13 +0000

    fake reset block

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-window.location.href = "https://rgms2.pages.dev/"
 
 // $('body').append('<script src="https://rusk-chat.pages.dev/chat-button.js"></script>');
 setTimeout(function() {
